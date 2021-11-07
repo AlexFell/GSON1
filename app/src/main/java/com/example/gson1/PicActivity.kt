0c728fa4d93046b94dca03be82a47b3d7aeb0c01
sasha <sasha.com1976@gmail.com>
2021-11-07 20:04:54 +0700

    new

--- app/src/main/java/com/example/gson1/PicActivity.kt
+++ app/src/main/java/com/example/gson1/PicActivity.kt
@@ -2,8 +2,11 @@ package com.example.gson1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 
 class PicActivity : AppCompatActivity() {
@@ -15,5 +18,22 @@ class PicActivity : AppCompatActivity() {
         val imageView = findViewById<View>(R.id.img) as ImageView
 
         Glide.with(this).load(pic).into(imageView)
+
+        val actionBar = supportActionBar
+        actionBar!!.title = "Photo"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.icon, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.action_one){
+            Toast.makeText(this, "Избранное", Toast.LENGTH_LONG).show()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
